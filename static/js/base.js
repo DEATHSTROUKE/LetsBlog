3aaa73ce3b216b5f0f05498dad45f11f3dabb594
unknown <andjeybur@gmail.com>
2020-04-19 01:51:53 +0300

    base template

--- static/js/base.js
+++ static/js/base.js
@@ -3,4 +3,6 @@ new WOW().init();
 window.addEventListener('load', function () {
     var menu = document.querySelector('.submenu');
     menu.style.transition = '.6s ease-in-out';
+    var menu1 = document.querySelector('.profile__sm');
+    menu.style.transition = '.6s ease-in-out';
 });
